a6d8a1135ad7dc33e891683c149a71f39b33bc08
IBXCODECAT <nathan.schmitt7@gmail.com>
2023-09-04 21:32:39 -0500

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,24 +7,20 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <a href="/">
-          <img src="https://i.imgur.com/4Q56XOx.png" alt="logo" />
+        <a href={window.location.origin}>
+          <img class="logo" src="https://avatars.githubusercontent.com/u/47279783?v=4" alt="logo" />
         </a>
       </div>
+      <h1>OpenIBX</h1>
       <ul className="nav-links">
-        <li><a href="/">Home</a></li>
+        <li><a href={window.location.origin}>OpenIBX</a></li>
+        <li>-</li>
         <li><a href="/explore">Explore</a></li>
+        <li>-</li>
         <li><a href="/notifications">Notifications</a></li>
+        <li>-</li>
         <li><a href="/profile">Profile</a></li>
       </ul>
-      <UserProfile user={{
-        username: "ibxcodecat",
-        bio: "I love cats!",
-        followers: 100,
-        following: 100,
-        profilePicture: "https://avatars.githubusercontent.com/u/47279783?v=4",
-        isVerified: true
-      }} />
     </nav>
   );
 };
